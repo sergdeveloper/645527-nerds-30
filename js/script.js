88a19f38abf0accf0e2a5081acc51986d5e93b35
sergdeveloper <serg.developer@yandex.ru>
2021-03-04 01:15:05 +0300

    Постарался доделать все, слайдер на главной, попап, отправки форм

---
+++ js/script.js
@@ -0,0 +1,55 @@
+const loginLink = document.querySelector(".map-button");
+const loginPopup = document.querySelector(".popup");
+const loginClose = loginPopup.querySelector(".popup-close");
+const loginForm = loginPopup.querySelector(".popup-form");
+const loginLogin = loginPopup.querySelector(".popup-name");
+const loginEmail = loginPopup.querySelector(".popup-email");
+let isStorageSupport = true;
+let storage = "";
+
+try {
+  storage = localStorage.getItem("login");
+} catch (err) {
+  isStorageSupport = false;
+}
+
+loginLink.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  loginPopup.classList.add("modal-show");
+
+  if (storage) {
+    loginLogin.value = storage;
+    loginEmail.focus();
+  } else {
+    loginLogin.focus();
+  }
+});
+
+loginClose.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  loginPopup.classList.remove("modal-show");
+  loginPopup.classList.remove("modal-error");
+});
+
+loginForm.addEventListener("submit", function (evt) {
+  if (!loginLogin.value || !loginEmail.value) {
+    evt.preventDefault();
+    loginPopup.classList.remove("modal-error");
+    loginPopup.offsetWidth = loginPopup.offsetWidth;
+    loginPopup.classList.add("modal-error");
+  } else {
+    if (isStorageSupport) {
+      localStorage.setItem("login", loginLogin.value);
+    }
+  }
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    if (loginPopup.classList.contains("modal-show")) {
+      evt.preventDefault();
+      loginPopup.classList.remove("modal-show");
+      loginPopup.classList.remove("modal-error");
+    }
+  }
+});
