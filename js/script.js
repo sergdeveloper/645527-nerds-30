191239d60892fbabc963c8c6693c462a33f54138
sergdeveloper <serg.developer@yandex.ru>
2021-03-14 20:42:17 +0300

    Множественные правки по резульататм проверки

--- js/script.js
+++ js/script.js
@@ -5,6 +5,46 @@ const form = popup.querySelector('.popup-form');
 const login = popup.querySelector('.user-name');
 const mail = popup.querySelector('.user-email');
 const storage = localStorage.getItem('user-name');
+const slideOne = document.querySelector('.slide-first');
+const slideTwo = document.querySelector('.slide-second');
+const slideThree = document.querySelector('.slide-third');
+const buttonFirst = document.querySelector('.first-button');
+const buttonSecond = document.querySelector('.second-button');
+const buttonThird = document.querySelector('.third-button');
+
+//slider
+
+buttonFirst.addEventListener('click', function (event) {
+  event.preventDefault();
+  slideOne.classList.add('slide-active');
+  buttonFirst.classList.add('active-button');
+  slideTwo.classList.remove('slide-active');
+  buttonSecond.classList.remove('active-button');
+  slideThree.classList.remove('slide-active');
+  buttonThird.classList.remove('active-button');
+});
+
+buttonSecond.addEventListener('click', function (event) {
+  event.preventDefault();
+  slideTwo.classList.add('slide-active');
+  buttonSecond.classList.add('active-button');
+  slideOne.classList.remove('slide-active');
+  buttonFirst.classList.remove('active-button');
+  slideThree.classList.remove('slide-active');
+  buttonThird.classList.remove('active-button');
+});
+
+buttonThird.addEventListener('click', function (event) {
+  event.preventDefault();
+  slideThree.classList.add('slide-active');
+  buttonThird.classList.add('active-button');
+  slideTwo.classList.remove('slide-active');
+  buttonSecond.classList.remove('active-button');
+  slideOne.classList.remove('slide-active');
+  buttonFirst.classList.remove('active-button');
+});
+
+//popup
 
 link.addEventListener('click', function (event) {
   event.preventDefault();
